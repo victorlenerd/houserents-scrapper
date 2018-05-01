70a492c6410a03120fefd4516beee5d9bf099941
sleekvick <nvonweb@outlook.com>
2018-05-01 16:33:21 +0100

    Restructure to add name keys

--- areas.js
+++ areas.js
@@ -1,317 +1,318 @@
-module.exports = [
-    "Abule Egba": {
+module.exports = [{
+        name: "Abule Egba"
         latLng: {
             lat: 6.6503,
             lng: 3.2860
         }
-    },
-    "Agbara-Igbesa": {
+    },{
+        name: "Agbara-Igbesa",
         latLng: {
             lat: 6.52861,
             lng: 3.135309
         }
-    },
-    "Agege": {
+    },{
+        name: "Agege",
         latLng: {
             lat: 6.615356,
             lng: 3.323782
         }
-    },
-    "Ajah": {
+    },{
+        name: "Ajah",
         latLng: {
             lat: 6.472516,
             lng: 3.568616
         }
     },
-    "Ajaokuta": {
+    {
+        name: "Ajaokuta",
         latLng: {
             lat: 7.558791,
             lng: 6.60178
         }
-    },
-    "Alagbado": {
+    },{
+        name: "Alagbado",
         latLng: {
             lat: 6.65844,
             lng: 3.263392
         }
-    },
-    "Alimosho": {
+    },{
+        name: "Alimosho",
         latLng: {
             lat: 6.546609,
             lng: 3.238251
         }
-    },
-    "Amuwo Odofin": {
+    },{
+        name: "Amuwo Odofin",
         latLng: {
             lat: 6.524379,
             lng: 3.379206
         }
-    },
-    "Apapa": {
+    },{
+        name: "Apapa",
         latLng: {
             lat: 6.444641,
             lng: 3.364084
         }
-    },
-    "Atlantic City": {
+    },{
+        name: "Atlantic City",
         latLng: {
             lat: 6.444116,
             lng: 3.372984
         }
-    },
-    "Badagry": {
+    },{
+        name: "Badagry",
         latLng: {
             lat: 6.431581,
             lng: 2.887644
         }
-    },
-    "Ebute Metta": {
+    },{
+        name: "Ebute Metta",
         latLng: {
             lat: 6.484464,
             lng: 3.376685
         }
-    },
-    "Egbe Idimu": {
+    },{
+        name: "Egbe Idimu",
         latLng: {
             lat: 6.564985,
             lng: 3.280998
         }
-    },
-    "Egbeda": {
+    },{
+        name: "Egbeda",
         latLng: {
             lat: 6.591574,
             lng: 3.291062
         }
-    },
-    "Ejigbo": {
+    },{
+        name: "Ejigbo",
         latLng: {
             lat: 6.55158,
             lng: 3.293578
         }
-    },
-    "Epe": {
+    },{
+        name: "Epe",
         latLng: {
             lat: 6.594595,
             lng: 3.977639
         }
-    },
-    "Gbagada": {
+    },{
+        name: "Gbagada",
         latLng: {
             lat: 6.561638,
             lng: 3.384247
         }
-    },
-    "Ijaiye": {
+    },{
+        name: "Ijaiye",
         latLng: {
             lat: 6.623306,
             lng: 3.331337
         }
-    },
-    "Ijede": {
+    },{
+        name: "Ijede",
         latLng: {
             lat: 6.570825,
             lng: 3.596457
         }
-    },
-    "Ibeju Lekki": {
+    },{
+        name: "Ibeju Lekki",
         latLng: {
             lat: 6.496267,
             lng: 3.596457
         }
-    },
-    "Idi Araba": {
+    },{
+        name: "Idi Araba",
         latLng: {
             lat: 6.517865,
             lng: 3.357785
         }
-    },
-    "Idimu": {
+    },{
+        name: "Idimu",
         latLng: {
             lat: 6.583887,
             lng: 3.243278
         }
-    },
-    "Iganmu": {
+    },{
+        name: "Iganmu",
         latLng: {
             lat: 6.484631,
             lng: 3.351486
         }
-    },
-    "Iju": {
+    },{
+        name: "Iju",
         latLng: {
             lat: 6.64974,
             lng: 3.323318
         }
-    },
-    "Ijaiye": {
+    },{
+        name: "Ijaiye",
         latLng: {
             lat: 6.623306,
             lng: 3.331337
         }
-    },
-    "Ikeja": {
+    },{
+        name: "Ikeja",
         latLng: {
             lat: 6.601838,
             lng: 3.351486
         }
-    },
-    "Ikorodu": {
+    },{
+        name: "Ikorodu",
         latLng: {
             lat: 6.619413,
             lng: 3.510454
         }
-    },
-    "Ikotun": {
+    },{
+        name: "Ikotun",
         latLng: {
             lat: 6.562564,
             lng: 3.243278
         }
-    },
-    "Ikotun Igando": {
+    },{
+        name: "Ikotun Igando",
         latLng: {
             lat: 6.562564,
             lng: 3.243278
         }
-    },
-    "Ikoyi": {
+    },{
+        name: "Ikoyi",
         latLng: {
             lat: 6.45,
             lng: 3.433333
         }
-    },
-    "Ilaje": {
+    },{
+        name: "Ilaje",
         latLng: {
             lat: 6.531235,
             lng: 3.394971
         }
-    },
-    "Ilashe": {
+    },{
+        name: "Ilashe",
         latLng: {
             lat: 6.4042593,
             lng: 3.295804
         }
-    },
-    "Ilupeju": {
+    },{
+        name: "Ilupeju",
         latLng: {
             lat: 6.553648,
             lng: 3.356674
         }
-    },
-    "Ipaja": {
+    },{
+        name: "Ipaja",
         latLng: {
             lat: 6.61307,
             lng: 3.265907
         }
-    },
-    "Isheri": {
+    },{
+        name: "Isheri",
         latLng: {
             lat: 6.639043,
             lng: 3.361598
         }
-    },
-    "Isolo": {
+    },{
+        name: "Isolo",
         latLng: {
             lat: 6.535498,
             lng: 3.308678
         }
-    },
-    "Imota": {
+    },{
+        name: "Imota",
         latLng: {
             lat: 6.663608,
             lng: 3.669929
         }
-    },
-    "Itire": {
+    },{
+        name: "Itire",
         latLng: {
             lat: 6.506057,
             lng: 3.331337
         }
-    },
-    "Ketu": {
+    },{
+        name: "Ketu",
         latLng: {
             lat: 6.596235,
             lng: 3.391811
         }
-    },
-    "Kosofe": {
+    },{
+        name: "Kosofe",
         latLng: {
             lat: 6.598781,
             lng: 3.409463
         }
-    },
-    "Kosofe Ikosi": {
+    },{
+        name: "Kosofe Ikosi",
         latLng: {
             lat: 6.60068,
             lng: 3.383047
         }
-    },
-    "Lagos Island": {
+    },{
+        name: "Lagos Island",
         latLng: {
             lat: 6.4549,
             lng: 3.4246
         }
-    },
-    "Lekki": {
+    },{
+        name: "Lekki",
         latLng: {
             lat: 6.4698,
             lng: 3.5852
         }
-    },
-    "Magodo": {
+    },{
+        name: "Magodo",
         latLng: {
             lat: 6.617909,
             lng: 400106
         }
-    },
-    "Maryland": {
+    },{
+        name: "Maryland",
         latLng: {
             lat: 6.576421,
             lng: 3.365344
         }
-    },
-    "Mushin": {
+    },{
+        name: "Mushin",
         latLng: {
             lat: 6.5352,
             lng: 3.3490
         }
-    },
-    "Ogba": {
+    },{
+        name: "Ogba",
         latLng: {
             lat: 6.6232,
             lng: 3.3464
         }
-    },
-    "Ogudu": {
+    },{
+        name: "Ogudu",
         latLng: {
             lat: 6.5749,
             lng: 3.3918
         }
-    },
-    "Ojo": {
+    },{
+        name: "Ojo",
         latLng: {
             lat:6.4619,
             lng: 3.1579
         }
-    },
-    "Ojodu": {
+    },{
+        name: "Ojodu",
         latLng: {
             lat: 6.6337,
             lng: 3.3573
         }
-    },
-    "Ojota": {
+    },{
+        name: "Ojota",
         latLng: {
             lat: 6.5856,
             lng: 3.3817
         }
-    },
-    "Oke Odo": {
+    },{
+        name: "Oke Odo",
         latLng: {
             lat: 6.6263,
             lng: 3.2835
         }
-    },
-    "Okota": {
+    },{
+        name: "Okota",
         latLng: {
             lat: 6.5088,
             lng: 3.3137
@@ -322,38 +323,39 @@ module.exports = [
             lat: 6.4780,
             lng: 3.3427
         }
-    },
-    "Oshodi": {
+    },{
+        name: "Oshodi",
         latLng: {
             lat: 6.5355,
             lng: 3.3087
         }
-    },
-    "Sangotedo": {
+    },{
+        name: "Sangotedo",
         latLng: {
             lat: 6.4535,
             lng: 3.6268
         }
-    },
-    "Shomolu": {
+    },{
+        name: "Shomolu",
         latLng: {
             lat: 6.5350,
             lng: 3.3893
         }
     },
-    "Surulere": {
+    {
+        name: "Surulere",
         latLng: {
             lat: 6.4926,
             lng: 3.3490
         }
-    },
-    "Victoria Island": {
+    },{
+        name: "Victoria Island",
         latLng: {
             lat: 6.4281,
             lng: 3.4219
         }
-    },
-    "Yaba": {
+    },{
+        name: "Yaba",
         latLng: {
             lat: 6.5005,
             lng: 3.3666
