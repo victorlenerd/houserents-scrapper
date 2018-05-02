a358df3598aeece63c64d9bfe94eff677a2afab9
sleekvick <nvonweb@outlook.com>
2018-05-02 19:27:38 +0100

    Include check for unknow address

--- adderssToLatLng.js
+++ adderssToLatLng.js
@@ -25,6 +25,15 @@ function tokenize(address) {
     return splits.filter(w => w.length > 1);
 }
 
+function localitySearch(localities) {
+    let mainLocality = search(localities, locality);
+    if (mainLocality) {
+        return mainLocality.latLng;
+    }
+
+    return null;
+}
+
 function addressLatLng(addess) {
     let addressTokens = tokenize(address);
     let addessParts = addressTokens.slice(0, addressTokens.length - 1);
@@ -33,19 +42,24 @@ function addressLatLng(addess) {
 
     if (existingArea && addessParts >= 2) {
         let locality = addessParts[addessParts.length - 2];
-        let mainLocality = search(areaLocalities[existingArea.name].localities, locality);
+        let mainLocality = localitySearch(areaLocalities[existingArea.name].localities, locality);
         if (mainLocality) {
             return mainLocality.latLng;
         } else  {
             // Geocode state + area + locality and store to localities
         }
-    } else if (existingArea) {
+    } else if (existingArea && addessParts <= 1) {
         return existingArea.latLng;
     } else {
         // Geocode address and store to unkownareas
+        let unknownArea = search(unkownAreas, mainArea);
+
+        if (unknownArea && addessParts >= 2) {
+            let locality = addessParts[addessParts.length - 2];
+            let mainLocality = localitySearch(areaLocalities[unknownArea.name].localities, locality);
+        }
     }
 }
-
 
 module.exports = function (addresses) {
     for (let i=0; i<addresses.length: i++) {
